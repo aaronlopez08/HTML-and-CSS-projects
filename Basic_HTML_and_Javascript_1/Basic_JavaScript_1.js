380421ddf7342538cebf67fe6a8667ba6aef3b5e
aaronlopez08 <138164477+aaronlopez08@users.noreply.github.com>
2023-08-02 17:02:18 -0500

    Add files via upload

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Java Alert!");
+function My_First_Functional(){
+		var string = "kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
